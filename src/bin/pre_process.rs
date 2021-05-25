4934aa52a0e88a485a53bb3a42a87cf75aa44d42
Masterchef365 <duncan.freeman1@gmail.com>
2021-05-25 13:51:57 -0700

    Preproc for train and test

--- src/bin/pre_process.rs
+++ src/bin/pre_process.rs
@@ -1,16 +1,64 @@
 use anyhow::Result;
-use programming_assn_3::{read_vocab, read_dataset};
+use programming_assn_3::{read_vocab, read_dataset, Vocabulary, Dataset};
 use std::fs::File;
-use std::io::{BufReader, Seek, SeekFrom};
+use std::io::{BufReader, BufWriter, Write, Seek, SeekFrom};
 
 fn main() -> Result<()> {
+    // Training data
     let mut training_file = BufReader::new(File::open("trainingSet.txt")?);
-
     let training_vocab = read_vocab(&mut training_file)?;
-    dbg!(training_vocab);
 
     training_file.seek(SeekFrom::Start(0))?;
-    //let training_vocab = read_dataset(&mut training_file, &training_vocab)?;
+    let training_data = read_dataset(&mut training_file, &training_vocab)?;
+
+    let mut preprocessed_train = BufWriter::new(File::create("preprocessed_train.txt")?);
+    write_features(&mut preprocessed_train, &training_data, &training_vocab)?;
+    drop((training_data, training_file));
+
+    // Test data
+    let mut test_file = BufReader::new(File::open("testSet.txt")?);
+    let test_data = read_dataset(&mut test_file, &training_vocab)?;
+
+    let mut preprocessed_test = BufWriter::new(File::create("preprocessed_test.txt")?);
+    write_features(&mut preprocessed_test, &test_data, &training_vocab)?;
+
+    Ok(())
+}
+
+fn write_features(file: &mut impl Write, dataset: &Dataset, vocab: &Vocabulary) -> Result<()> {
+    // Write vocab
+    for word in vocab {
+        write!(file, "{},", word)?;
+    }
+    writeln!(file)?;
+
+    // Write rows - essentially converting to a dense representation from a sparse one
+    for row in dataset {
+        let mut pos = 0;
+
+        // Write features with zeroes in between
+        for &feature in &row.features {
+            while pos != feature {
+                write!(file, "0,")?;
+                pos += 1;
+            }
+            write!(file, "1,")?;
+            pos += 1;
+        }
+
+        // Write remaining zeroes
+        while pos < vocab.len() {
+            write!(file, "0,")?;
+            pos += 1;
+        }
+
+        if row.class {
+            write!(file, "1")?; 
+        } else {
+            write!(file, "0")?; 
+        }
+        writeln!(file)?;
+    }
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use anyhow::{bail, Context, Result};
 use std::collections::HashSet;
-use std::io::{BufRead, Read};
+use std::io::BufRead;
 
 /// A vocabulary consists of all words in alphabetical order
 pub type Vocabulary = Vec<String>;
@@ -9,9 +9,10 @@ pub type Vocabulary = Vec<String>;
 pub type Dataset = Vec<Row>;
 
 /// A row consists of a set of indices into an associated alphabet
+#[derive(Debug, Clone)]
 pub struct Row {
     /// Set of indices into an associated alphabet, excluding those not present in the alphabet
-    pub words: Vec<usize>,
+    pub features: Vec<usize>,
     /// True if the class for this row was "1", and false if it was "0"
     pub class: bool,
 }
@@ -41,23 +42,48 @@ pub fn read_vocab(reader: &mut impl BufRead) -> Result<Vocabulary> {
     Ok(vocab)
 }
 
-/// Read the dataset, assigning
+/// Read the dataset into a set of rows, using the vocab for feature indices
 pub fn read_dataset(reader: &mut impl BufRead, vocab: &[String]) -> Result<Dataset> {
-    /*
+    let mut dataset = vec![];
+
     for (line_idx, line) in reader.lines().enumerate() {
+        // Split line into parts
+        let line = line?;
+        let mut parts: Vec<&str> = line.split_whitespace().collect();
+
         // Parse class
-        let missing_class = || format!("Line {} missing class", line_idx+1);
+        let missing_class = || format!("Line {} missing class", line_idx + 1);
         let class = parts.pop().with_context(missing_class)?;
-        let class = match *class {
+        let class = match class {
             "1" => true,
             "0" => false,
             _ => bail!("{}", missing_class()),
         };
-    */
 
-    todo!()
+        // Segment words and add them to the vocab, sorting them in order
+        let mut features = vec![];
+        for word in line.split_whitespace() {
+            let word = prepare_word(word);
+            if word.is_empty() {
+                continue;
+            }
+            match vocab.binary_search(&word) {
+                Ok(idx) => features.push(idx),
+                Err(_) => continue, // Note: We skip words not in the vocab!
+            };
+        }
+
+        // Sort and dedup for later searching
+        features.sort_unstable();
+        features.dedup();
+
+        dataset.push(Row { features, class });
+    }
+
+    Ok(dataset)
 }
 
+/// Prepare a word
 pub fn prepare_word(word: &str) -> String {
     word.chars().filter(char::is_ascii_alphanumeric).collect()
 }
